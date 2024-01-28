10c53434623f5a8076c31c1d3fdb50d955491838
Fiza Andani <fandani484@rku.ac.in>
2024-01-28 13:23:31 +0530

    28/1/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:spark/views/splash_screen/splash_screen.dart';
+
 import 'consts/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
